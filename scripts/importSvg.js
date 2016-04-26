8df4dbbad727d9f0b1bb890c658adffbbf1eacd9
hartgeld <lorehartgeld@gmail.com>
2016-04-26 19:55:30 +0200

    lazyLoad, sizeControl

--- scripts/importSvg.js
+++ scripts/importSvg.js
@@ -1,13 +1,14 @@
 
 jQuery(document).ready(function(){
 
-	var letters = [  "__U+0020", "__U+0021", "__U+0022", "__U+0023", "__U+0024", "__U+0025", "__U+0026", "__U+0027", "__U+0028", "__U+0029", "__U+002a", "__U+002b", "__U+002c", "__U+002d", "__U+002e", "__U+002f", "__U+0030", "__U+0031", "__U+0032", "__U+0033", "__U+0034", "__U+0035", "__U+0036", "__U+0037", "__U+0038", "__U+0039", "__U+003a", "__U+003b", "__U+003c", "__U+003d", "__U+003e", "__U+003f", "__U+0040", "__U+0041", "__U+0042", "__U+0043", "__U+0044", "__U+0045", "__U+0046", "__U+0047", "__U+0048", "__U+0049", "__U+004a", "__U+004b", "__U+004c", "__U+004d", "__U+004e", "__U+004f", "__U+0050", "__U+0051", "__U+0052", "__U+0053", "__U+0054", "__U+0055", "__U+0056", "__U+0057", "__U+0058", "__U+0059", "__U+005a", "__U+005b", "__U+005c", "__U+005d", "__U+005e", "__U+005f" , "__U+007b", "__U+007c", "__U+007d", "__U+007e", "__U+2018"] // 
+	var letters = [  "0020", "0021", "0022", "0023", "0024", "0025", "0026", "0027", "0028", "0029", "002a", "002b", "002c", "002d", "002e", "002f", "0030", "0031", "0032", "0033", "0034", "0035", "0036", "0037", "0038", "0039", "003a", "003b", "003c", "003d", "003e", "003f", "0040", "0041", "0042", "0043", "0044", "0045", "0046", "0047", "0048", "0049", "004a", "004b", "004c", "004d", "004e", "004f", "0050", "0051", "0052", "0053", "0054", "0055", "0056", "0057", "0058", "0059", "005a", "005b", "005c", "005d", "005e", "005f", "007b", "007c", "007d", "007e", "2018" ]
 	var myCounter = 0;
+	
 
 	for (var letterCount = 0; letterCount < letters.length; letterCount++) {
 
 		var letterName = letters[letterCount];
-		var myUrl = "/home/hartgeld/Websites/importSvg/source/" + letterName + ".svg";
+		var myUrl = "/home/hartgeld/Websites/importSvg/source/" + "__U+" + letterName + ".svg";
 
 		$.ajax({
 	  	    type: "GET",
@@ -25,6 +26,36 @@ jQuery(document).ready(function(){
 				document.body.appendChild(myLetterContainer);
 				myLetterContainer.id = letters[myCounter]; 
 
+				var canvas;
+				var mySlider = document.getElementById('mySlider');
+				var scaleFactor = mySlider.value;
+				console.log("init scaleFactor: " + scaleFactor);
+				
+				var isScaled = false;
+				console.log(isScaled);
+				
+				mySlider.onchange = function(){
+					scaleFactor = mySlider.value;
+					console.log("new scaleFactor: " + scaleFactor);
+					isScaled = true;
+					canvasResize();
+				};
+
+				var myp5 = null;
+				var myp5GlobalSketchVar = null;
+
+				var canvasResize = function() {
+				    if(myp5 && myp5GlobalSketchVar) {
+				    	myp5.resizeCanvas(width*scaleFactor, height*scaleFactor);
+				    	console.log('canvasresize: ' + width*scaleFactor + ', ' + height*scaleFactor );
+
+				    	var myCanvasDiv = "#"+letterName;
+				    	console.log(myCanvasDiv);
+				    	$("#"+letterName).load(document.URL +  " #"+letterName);
+				    	window.location.reload(false); 
+				    }
+				}
+
 				var sketch = function( p ) {
 
 					var physics;
@@ -41,11 +72,14 @@ jQuery(document).ready(function(){
 
 					var myLetterUnicode;
 
+					var mousePos;
+					var mousePosDrag;
+					var mouseAttractor;
+
 					p.setup = function() {
 
-						p.createCanvas(width/2, height/2);
-
-						center = new Vec2D(width/2, height/2);
+						//canvas = p.createCanvas(width*scaleFactor, height*scaleFactor);
+						myp5GlobalSketchVar = p.createCanvas(width*scaleFactor, height*scaleFactor);
 
 						p.background(0,255,255);
 
@@ -53,12 +87,63 @@ jQuery(document).ready(function(){
 						physics.setWorldBounds(new Rect(1,1,width-1,height-1));
 						physics.addBehavior(new GravityBehavior(new Vec2D(0,0)));
 
-						console.log('loading letter ' + letters[myCounter]);
 						myLetterUnicode = letters[myCounter];
 
+						console.log('loading letter ' + myLetterUnicode + ' |width: ' + width*scaleFactor + ' |height: ' + height*scaleFactor);
+
+						p.buildLetter();						
+
+						//remove all svg from dom 
+						var element = document.getElementsByTagName("svg");
+						for (index = element.length - 1; index >= 0; index--) {
+						    element[index].parentNode.removeChild(element[index]);
+						}
+
+						p.lazyload();
+
+						mousePos = new Vec2D(p.mouseX, p.mouseY);
+						mouseAttractor = new AttractionBehavior(mousePos, 1000, 1.9);
+						physics.addBehavior(mouseAttractor);
+						
+					}
+
+					p.draw = function(){
+						
+						if(initLazyLoad == true){
+
+							p.background(245);
+							physics.update();
+
+							for (var i=0; i<springsInLetter.length; i++ ){
+								var s = springsInLetter[i];
+								p.noFill();
+								p.strokeWeight(5);
+								p.line(s.a.x,s.a.y,s.b.x,s.b.y);
+							}
+
+							for(var i = 0;i<physics.particles.length; i++) {
+								var myParticle= physics.particles[i];
+								p.strokeWeight(2);
+								p.stroke(255);
+								p.fill(0);
+								p.ellipse(myParticle.x,myParticle.y,9,9);
+		  					}
+						}
+						else{
+							p.background(22,145,245);
+						}
+
+						mousePos.x = p.mouseX;
+						mousePos.y = p.mouseY;
+					}
+
+					$(window).scroll(function(){
+						p.lazyload();
+					})
+
+					p.buildLetter = function(){
+						
 						shape.forEach(function(e) {
-							//console.log('canvas dimensions |w:'+width+' |h: '+height);
-			      			//console.log('id:' + e.id + 'vectors: ' + e.attrs.path.length);
 
 			      			var counter=0;
 
@@ -69,7 +154,7 @@ jQuery(document).ready(function(){
 			      				if (typeof x !== "undefined" && typeof y !== "undefined") {
 
 			      					//console.log( ' |x:' + x + ' |y:' + y + ' |id:' + counter);
-			      					particlePosition = new Vec2D(x/2, y/2);
+			      					particlePosition = new Vec2D(x*scaleFactor, y*scaleFactor);
 			      					initParticlePosition = new Vec2D(0, 0);
 			      					var particle = new VerletParticle2D(particlePosition);
 			      					var particleFixed = new VerletParticle2D(particlePosition);
@@ -88,8 +173,8 @@ jQuery(document).ready(function(){
 			      						//physics.addSpring(new VerletSpring2D(previousParticle, particle, distance, 0.1));
 			      					} else {
 			      						var lastParticleSource = e.attrs.path[e.attrs.path.length - 2]
-			      						var lastParticlePositionX = lastParticleSource[1]/2;
-			      						var lastParticlePositionY = lastParticleSource[2]/2;
+			      						var lastParticlePositionX = lastParticleSource[1]*scaleFactor;
+			      						var lastParticlePositionY = lastParticleSource[2]*scaleFactor;
 			      						var lastParticlePosition = new Vec2D(lastParticlePositionX, lastParticlePositionY);
 			      						lastParticle = new VerletParticle2D(lastParticlePosition);
 			      						physics.addParticle(lastParticle);
@@ -113,9 +198,7 @@ jQuery(document).ready(function(){
 			      			});
 						});
 
-						
-
-						//console.log('particles_total: ' + physics.particles.length);
+						// connect particles with equal positions
 						var pi, pj;
 						for (var i = 0; i < physics.particles.length; i++){	
 							pi = physics.particles[i];
@@ -131,51 +214,78 @@ jQuery(document).ready(function(){
 								}
 							}
 						}
-
-						//remove all svg from dom 
-						var element = document.getElementsByTagName("svg");
-						for (index = element.length - 1; index >= 0; index--) {
-						    element[index].parentNode.removeChild(element[index]);
-						}
-
-						
-						p.lazyload();
-						
 					}
 
-					p.draw = function(){
-
-						p.background(245);
-
-						//console.log( myCounter + " |initLazyLoad: " + initLazyLoad);
+					p.lazyload = function(){
 						
-						if(initLazyLoad == true){
-							
-							//console.log("is visib");
-							physics.update();
+						var wt = $(window).scrollTop();    	//* top of the window
+						var wb = wt + $(window).height();  	//* bottom of the window
+						var ot = $(document.getElementById(myLetterUnicode)).offset().top;
+						var ob = $(document.getElementById(myLetterUnicode)).height(); 	//* bottom of object
 
-							for (var i=0; i<springsInLetter.length; i++ ){
-								var s = springsInLetter[i];
-								p.noFill();
-								p.strokeWeight(5);
-								p.line(s.a.x,s.a.y,s.b.x,s.b.y);
-
-							}
-
-							for(var i = 0;i<physics.particles.length; i++) {
-								var myParticle= physics.particles[i];
-								p.strokeWeight(2);
-								p.stroke(255);
-								p.fill(0);
-								p.ellipse(myParticle.x,myParticle.y,9,9);
-		  					}
-						}
-						else{
-							p.background(22,145,245);
-							//initLazyLoad = undefined;
+						initLazyLoad = false;
+						
+						if( wt<=ot || wb >= ob ){
+						    initLazyLoad = true;
 						}
 
-	  					// ladzy load snip
+						if( wb<=ot ){
+						    initLazyLoad = false;
+						}		
+						if(wt>=ot+height*scaleFactor){
+							initLazyLoad = false;
+						}			
+					}
+
+					p.mousePressed = function() {
+					  
+					  selected=null;
+					  mousePosDrag = new Vec2D(p.mouseX, p.mouseY);
+					  for(var i = 0; i<physics.particles.length;i++) {
+					    var myParticle = physics.particles[i];
+					    if (myParticle.distanceToSquared(mousePos)<snapDist) {
+					      selected=myParticle;
+					      selected.lock();
+					      break;
+					    }   
+					  }					  
+					}
+
+					p.mouseDragged = function() {
+					  if (selected!=null) {
+					    selected.set(p.mouseX,p.mouseY);
+					  }
+					}
+
+					p.mouseReleased = function() {
+					  if (selected!=null) {
+					    selected.unlock();
+					    selected=null;
+					  }
+					}
+
+					var elt = document.getElementById(letters[myCounter]);
+					var letterInfo = p.createDiv( "<p>" + "&#x" + letters[myCounter] + "&#x0020" + "U+" + letters[myCounter] + "</p>" );
+					letterInfo.class("letterInfo");
+					letterInfo.parent(elt);
+				}
+				
+		    	var containerNode = document.getElementById(letters[myCounter]);
+				myp5 = new p5(sketch, containerNode);
+				myCounter++;
+
+		    }
+		});
+	};
+});
+
+
+
+/////////////
+/* BACKUPZ */
+/////////////
+
+// ladzy load snip
 	  					
 	  					/*$(document).ready(function(){
 						   $(window).scroll(lazyload);
@@ -200,11 +310,18 @@ jQuery(document).ready(function(){
 						}
 						*/
 
-					}
+												/*
+						if(  wt<=ob && wb >= ot){ 
+						    console.log(myLetterUnicode + "is visible");
+						    initLazyLoad = true;    
+						} */
+						/*
+						if( wt<=ot && wb >= ob ){
+						    initLazyLoad = true;
+						    console.log(" wt<=ot && wb >= ob");
+						}*/
 
-					//$(window).scroll(p.lazyload);
-					$(window).scroll(function(){
-						p.lazyload();
+
 													//
 
 						/*var wt = $(window).scrollTop();    	//* top of the window
@@ -220,75 +337,3 @@ jQuery(document).ready(function(){
 							initLazyLoad = false;
 							$(document.getElementById(myLetterUnicode)).attr("loaded",false);
 						}*/
-					})
-
-					p.lazyload = function(){
-						
-						var ot = $(document.getElementById(myLetterUnicode)).offset().top;
-						var wt = $(window).scrollTop();    	//* top of the window
-						var wb = wt + $(window).height();  	//* bottom of the window
-						
-						//var ot = $(canvasContainer).offset().top;  	//* top of object (i.e. advertising div)
-						var ob = $(document.getElementById(myLetterUnicode)).height(); 	//* bottom of object
-						console.log("LazyLoad.getDivPositions |wt: " + wt + " |wb: " + wb + " |ot: " + ot + " |ob: " + ob);
-
-						initLazyLoad = false;
-						
-						/*
-						if(  wt<=ob && wb >= ot){ 
-						    console.log(myLetterUnicode + "is visible");
-						    initLazyLoad = true;    
-						} */
-
-						if( wt<=ot && wb >= ob ){
-						    initLazyLoad = true;
-						}
-
-						if( wb<=ot){
-						    initLazyLoad = false;
-						}
-						
-					}
-
-					p.mousePressed = function() {
-					  selected=null;
-					  mousePos = new Vec2D(p.mouseX, p.mouseY);
-					  for(var i = 0; i<physics.particles.length;i++) {
-					    var myParticle = physics.particles[i];
-					    if (myParticle.distanceToSquared(mousePos)<snapDist) {
-					      selected=myParticle;
-					      selected.lock();
-					      break;
-					    }   
-					  }
-					}
-
-					p.mouseDragged = function() {
-					  if (selected!=null) {
-					    selected.set(p.mouseX,p.mouseY);
-					  }
-					}
-
-					p.mouseReleased = function() {
-					  if (selected!=null) {
-					    selected.unlock();
-					    selected=null;
-					  }
-					}
-
-					var elt = document.getElementById(letters[myCounter]);
-					var letterInfo = p.createDiv( "<p>" + letters[myCounter] + "</p>" );
-					letterInfo.class("letterInfo");
-					letterInfo.parent(elt);
-
-				}
-				
-		    	var containerNode = document.getElementById(letters[myCounter]);
-				var myp5 = new p5(sketch, containerNode);
-				myCounter++;
-
-		    }
-		});
-	};
-});
-
